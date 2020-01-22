443c54df66d25c4b191de6e197a109632b1eefae
Poonam <Poonamdhimate@gmail.com>
2020-01-22 12:49:28 +1100

    fixed build error

--- IdentityAPI/Startup.cs
+++ IdentityAPI/Startup.cs
@@ -28,8 +28,6 @@ namespace IdentityAPI
         public IConfiguration Configuration { get; }
 
         private IServiceProvider ServiceProvider { get; set; }
-        public static readonly ILoggerFactory MyLoggerFactory
-    = LoggerFactory.Create(builder => { builder.; });
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
